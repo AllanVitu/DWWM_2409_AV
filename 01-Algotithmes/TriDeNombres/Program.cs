fd4fa34f348dbdd0bd968247f5b29fe3c26ac6d2
Drawk <166002323+AllanVitu@users.noreply.github.com>
2024-09-23 12:35:54 +0200

    TriDeNombres premier essaie

--- 01-Algotithmes/TriDeNombres/Program.cs
+++ 01-Algotithmes/TriDeNombres/Program.cs
@@ -4,7 +4,64 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int a;
+
+            int b;
+
+            int c;
+
+
+            Console.WriteLine("Saisissez un nombre");
+
+            a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Saisissez un nombre");
+
+            b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Saisissez un nombre");
+
+            c = Convert.ToInt32(Console.ReadLine());
+
+
+            static String LesNombresDeMort(int a, int b, int c)
+            {
+                String result;
+
+                if (a < b && b < c)
+                {
+                    result = (a + " " + b + " " + c);
+                }
+                else if (a < c && c < b)
+                {
+                    result = (a + " " + c + " " + b);
+                }
+                else if (b < a && a < c)
+                {
+                    result = (b + " " + c + " " + a);
+                }
+                else if (c < b && b < a)
+                {
+                    result = (c + " " + a + " " + b);
+                }
+                else if (c < b && a < c)
+                {
+                    result = (c + " " + b + " " + a);
+                }
+                else
+                {
+                    result = "La suite est invalide";
+                }
+
+                return result;
+            }
+
+
+
+
+
+
         }
-    }
+    }    
 }
+
