e28f7f9ab2f11b5b6eb8087c63068a4016c31658
Drawk <166002323+AllanVitu@users.noreply.github.com>
2024-09-23 12:42:37 +0200

    2èmes essaies de TriDeNombres

--- 01-Algotithmes/TriDeNombres/Program.cs
+++ 01-Algotithmes/TriDeNombres/Program.cs
@@ -10,9 +10,9 @@
 
             int c;
 
-
+            
             Console.WriteLine("Saisissez un nombre");
-
+           
             a = Convert.ToInt32(Console.ReadLine());
 
             Console.WriteLine("Saisissez un nombre");
@@ -24,44 +24,35 @@
             c = Convert.ToInt32(Console.ReadLine());
 
 
-            static String LesNombresDeMort(int a, int b, int c)
+            String result;
+
+            if (a < b && b < c)
             {
-                String result;
-
-                if (a < b && b < c)
-                {
-                    result = (a + " " + b + " " + c);
-                }
-                else if (a < c && c < b)
-                {
-                    result = (a + " " + c + " " + b);
-                }
-                else if (b < a && a < c)
-                {
-                    result = (b + " " + c + " " + a);
-                }
-                else if (c < b && b < a)
-                {
-                    result = (c + " " + a + " " + b);
-                }
-                else if (c < b && a < c)
-                {
-                    result = (c + " " + b + " " + a);
-                }
-                else
-                {
-                    result = "La suite est invalide";
-                }
-
-                return result;
+                Console.WriteLine(a + " " + b + " " + c);
             }
-
-
-
-
-
-
+            else if (a < c && c < b)
+            {
+                Console.WriteLine(a + " " + c + " " + b);
+            }
+            else if (b < a && a < c)
+            {
+                Console.WriteLine(b + " " + c + " " + a);
+            }
+            else if (c < b && b < a)
+            {
+                Console.WriteLine(c + " " + a + " " + b);
+            }
+            else if (c < b && a < c)
+            {
+                Console.WriteLine(c + " " + b + " " + a);
+            }
+            else
+            {
+                result = "La suite est invalide";
+            }
+                
+            
+            
         }
-    }    
+    }
 }
-
