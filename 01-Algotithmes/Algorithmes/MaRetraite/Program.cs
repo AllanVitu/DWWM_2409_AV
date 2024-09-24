fe9099cef58e210a23f859eed20a4bcc76dedb60
Drawk <166002323+AllanVitu@users.noreply.github.com>
2024-09-24 15:44:55 +0200

    Correction Exercice maRetraite

--- 01-Algotithmes/Algorithmes/MaRetraite/Program.cs
+++ 01-Algotithmes/Algorithmes/MaRetraite/Program.cs
@@ -20,28 +20,22 @@
             {
                 ageDiff = numberA - ageDeLaRetraite;
                 Console.WriteLine("Vous êtes à la retraite depuis " + ageDiff + " années");
-                Console.ReadLine();
             }
             else if (numberA < ageDeLaRetraite)
             {
                 ageDiff = ageDeLaRetraite - numberA;
                 Console.WriteLine("Il vous reste " + ageDiff + " années avant la retraite.");
-                Console.ReadLine();
             }
             else if (numberA == ageDeLaRetraite)
             {
                 Console.WriteLine("C’est le moment de prendre sa retraite.");
-                Console.ReadLine();
-
+                
             }
             else
             {
                 Console.WriteLine("La valeur fournie n’est pas un âge valide.");
             }
             Console.ReadLine();
-            {
-                 
-            }
 
         }
     }
