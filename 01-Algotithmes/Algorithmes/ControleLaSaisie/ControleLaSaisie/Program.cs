8ea616c5cd9add9c022d6fda9c9e1b5e46316de9
Drawk <166002323+AllanVitu@users.noreply.github.com>
2024-09-26 15:29:48 +0200

    Debut et fin de l'exercice ControleLaSaisie et début Exercice intervalleEntreDeuxChiffres

---
+++ 01-Algotithmes/Algorithmes/ControleLaSaisie/ControleLaSaisie/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ControleLaSaisie
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+
+            Console.WriteLine("Saisissez votre prenom");
+            
+            name = Console.ReadLine();
+            
+            Console.WriteLine("Bonjour " + name);
+        }
+    }
+}
---
+++ 01-Algotithmes/Algorithmes/IntervalleEntreDeuxNombres/IntervalleEntreDeuxNombres/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace IntervalleEntreDeuxNombres
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int chA = 0;
+
+            int chB;
+
+            int result;
+
+
+            Console.WriteLine("Saisissez un chiffre : ");
+            Console.ReadLine();
+
+            if (chA == 1)
+            {
+                chB = 5;
+            }
+
+
+        }
+    }
+}
