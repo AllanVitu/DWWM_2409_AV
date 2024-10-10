fde10c54368d81d0e7f84769fd1f98017766b292
Drawk <166002323+AllanVitu@users.noreply.github.com>
2024-10-10 11:16:32 +0200

    Exercice utilisateurEnregistrer

---
+++ 01-Algotithmes/Algorithmes/JalonCarteCRM/JalonCarteCRM/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace JalonCarteCRM
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string badge;
+
+            string[] tabUtilisateur;
+
+            int prixRepas = 4;
+
+
+
+            tabUtilisateur = new string[] { "Allan", "Marie", "Remi", "Wafae", "Julien" };
+
+            Console.WriteLine("Veuillez saisir votre badge");
+
+            Console.ReadLine();
+
+            for (int i = 0; i < tabUtilisateur.Length; i++)
+            {
+                //Désolé de ne pas avoir fini mais voici ma logique :
+
+                //Je voulais que passer ma Variable badge
+            }  
+           
+        }
+    }
+}
---
+++ 01-Algotithmes/Algorithmes/utilisateurEnregistrer/utilisateurEnregistrer/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace utilisateurEnregistrer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] utilisateurs;
+
+            string saisieNomPrenom;
+
+            char saisieOUiNon;
+
+            string[] tabTempo;
+
+
+            utilisateurs = new string[] { };
+
+            do
+            {
+                Console.WriteLine("Saisissez votre nom et prenom :");
+
+                saisieNomPrenom = Console.ReadLine() ?? " ";
+
+                tabTempo = utilisateurs;
+
+                utilisateurs = new string[utilisateurs.Length + 1];
+
+                tabTempo.CopyTo(utilisateurs, 0);
+
+                utilisateurs[utilisateurs.Length - 1] = saisieNomPrenom;
+
+                Console.WriteLine("Souhaitez vous ajouter un autre utlisateur ? (N/O) ");
+
+                saisieOUiNon = Console.ReadKey().KeyChar;
+            }
+            
+            while (saisieOUiNon == 'o' || saisieOUiNon == 'O');
+        }
+    }
+}
