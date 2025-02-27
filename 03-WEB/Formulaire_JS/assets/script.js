2b94bd6991a4c46e95a5f8ca0a4c366b3bda0cb3
Drawk <166002323+AllanVitu@users.noreply.github.com>
2025-02-27 11:58:43 +0100

    Création et début de l'exercice Formulaire

---
+++ 03-WEB/Formulaire_JS/assets/script.js
@@ -0,0 +1,17 @@
+let prenomSaisie = document.querySelector("#saisiePrenom")
+let ageSaisie = document.querySelector("#saisieAge")
+let resultat = document.querySelector("#rsltFormulaires")
+const boutonValider = document.querySelector("#btnValider")
+const boutonVider = document.querySelector("#btnVider")
+let i
+
+
+
+
+const ssePrenom = () => {
+    if(prenomSaisie){
+
+    }
+}
+
+
