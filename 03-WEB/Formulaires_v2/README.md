ab57ca34d5995574be9a11eb8010ec4b6719e4d4
Drawk <166002323+AllanVitu@users.noreply.github.com>
2025-03-17 12:36:28 +0100

    Grosse update JS ! Merci Mickael !

--- 03-WEB/Formulaires_v2/README.md
+++ 03-WEB/Formulaires_v2/README.md
@@ -2,3 +2,9 @@
 2. Creer le CSS, JS et ou PHP [SPRED]
 3. Creer la structure HTML et CSS du formulaire
 4. Faire la BDD
+
+
+
+
+
+
--- 03-WEB/Formulaires_v2/assets/script.js
+++ 03-WEB/Formulaires_v2/assets/script.js
@@ -2,6 +2,8 @@
 const container = document.getElementById('container');
 const loginButton = document.getElementById('login');
 const signUpButton = document.getElementById('signUp');
+const btnNoCompte = document.getElementById('btnNoCompte');
+const btnOkCompte =  document.getElementById('btnOkCompte');
 
 // Ajoute la classe "active" pour afficher le formulaire d'inscription
 signUpButton.addEventListener('click', () => {
@@ -12,3 +14,17 @@ signUpButton.addEventListener('click', () => {
 loginButton.addEventListener('click', () => {
     container.classList.remove('active');
 });
+
+btnNoCompte.addEventListener('click', () =>{
+    let toto = document.querySelector('.container');
+
+    toto.classList.toggle('active');
+
+})
+
+btnOkCompte.addEventListener('click', () =>{
+    let toto = document.querySelector('.container');
+
+    toto.classList.toggle('active');
+
+})
