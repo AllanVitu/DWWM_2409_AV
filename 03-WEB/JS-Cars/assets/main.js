d136fe4df6f61b1d9d10bb5534f1c309f858fa95
Drawk <166002323+AllanVitu@users.noreply.github.com>
2025-03-19 10:44:58 +0100

    commencement du l'exo cars

--- 03-WEB/JS-Cars/assets/main.js
+++ 03-WEB/JS-Cars/assets/main.js
@@ -1,3 +1,16 @@
+const nomVoiture = document.getElementById('#carName');
 const btnRecherche = document.getElementById('#btnRecherche');
+const infoVoiture = document.getElementById('#result');
+
+fetch('https://raw.githubusercontent.com/ARFP/DWWM_2409/refs/heads/develop/02%20-%20webfront/JS/exo-js-cars/data/cars.json')
+.then((Response)=>{
+    return Response.json();
+}).then((data)=>{
+    console.log(data);
+}).catch((error) => alert("Erreur : " + error));
+
+function afficher(_nomVoiture) {
+    
+}
 
 
---
+++ 03-WEB/exo-js-cars/main.js
@@ -0,0 +1,14 @@
+const validate = document.querySelector("#validate")
+
+const inputCarName = document.querySelector("#carName")
+
+const result = document.querySelector("#result")
+
+const carChoice = document.querySelector("#car-choice")
+
+import {collectionCars} from './data/cars.js'
+
+console.log(collectionCars);
+
+
+
