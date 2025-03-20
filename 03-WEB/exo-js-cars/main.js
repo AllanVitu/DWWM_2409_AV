255117877f7098207d5c1b46b1587326feab2070
Drawk <166002323+AllanVitu@users.noreply.github.com>
2025-03-20 15:43:36 +0100

    Update JS exoCars

--- 03-WEB/exo-js-cars/main.js
+++ 03-WEB/exo-js-cars/main.js
@@ -1,14 +1,49 @@
+import {collectionCars} from './data/cars.js'
+
 const validate = document.querySelector("#validate")
 
 const inputCarName = document.querySelector("#carName")
 
-const result = document.querySelector("#result")
+const ulElement = document.getElementById("ul")
 
-const carChoice = document.querySelector("#car-choice")
 
-import {collectionCars} from './data/cars.js'
+validate.addEventListener('click', function(event) {
+    event.preventDefault();
+    // recuéprer la veleur de inputCarName
+    let search = inputCarName.value.toLowerCase();
+    let list = collectionCars;
 
-console.log(collectionCars);
+    // rechercher dans la collection collectionCars les voitures dont le nom correspond
+   let mesVoitures = list.filter(
+    uneVoiture => uneVoiture.car_name.toLowerCase().includes(search)
+   )
+
+   console.log(mesVoitures);
+
+   for (const car of mesVoitures) {
+        const liElement = document.createElement('li');
+        liElement.innerText = car.car_name;
+        ulElement.appendChild(liElement)
+    }
+})
+
+
+// il faut afficher ce qu'il y a au dessus et qu'il s'affiche dans la page html
+/*const ulElement = document.getElementById("ul")
+
+fetch('./data/cars.json').then(response =>{
+    return response.json();
+})
+.then((cars) => {
+    
+    console.log(cars)
+    for (const car of cars) {
+        const liElement = document.createElement('li');
+        liElement.innerText = car.car_name;
+        ulElement.appendChild(liElement)
+    }
+});*/
+
 
 
 
