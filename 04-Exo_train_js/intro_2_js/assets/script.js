9ec16839206a8d522d1234bcd631e73b40f71f66
Drawk <166002323+AllanVitu@users.noreply.github.com>
2025-03-24 14:09:19 +0100

    update

--- 04-Exo_train_js/intro_2_js/assets/script.js
+++ 04-Exo_train_js/intro_2_js/assets/script.js
@@ -2,7 +2,8 @@ const btnAugmenter = document.querySelector('#btnAugementer')
 const btnDiminuer = document.querySelector('#btnDiminu')
 const zonePx = document.querySelector('#zoneText')
 const text = document.querySelector('#text')
-const manouelle = document.getElementById('#saisieManouelle')
+const manouelle = document.getElementById('saisieManouelle')
+
 
 
 let taille = 16;
@@ -41,5 +42,13 @@ btnDiminuer.addEventListener('click', diminution);
 
 
 const saisie = () => {
-    
+    var result = manouelle.value
+    if(result < 8 || result > 48){
+        result = 16
+    }
+    taille = result
+    zonePx.innerText = taille + "px"
+    text.style.fontSize = taille + "px"
 }
+
+manouelle.addEventListener('input', saisie)
