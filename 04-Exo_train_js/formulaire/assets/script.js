0d95f2643bbd0af15d9bee59afb1f93e4d01e82e
Drawk <166002323+AllanVitu@users.noreply.github.com>
2025-03-27 09:22:04 +0100

    formulaires commencer

---
+++ 04-Exo_train_js/formulaire/assets/script.js
@@ -0,0 +1,11 @@
+const prenom = document.querySelector('#chmpPrenom')
+const age = document.querySelector('#chmpAge')
+const btnValider = document.querySelector('#btnValider')
+const btnVider = document.querySelector('#btnVider')
+const result = document.querySelector('#result')
+
+const champ = () => {
+    if(!prenom && age > 0 ){
+        
+    }
+}
