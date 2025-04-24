4726ff663a22e2b282b89f1278088b2111a0e951
Drawk <166002323+AllanVitu@users.noreply.github.com>
2025-04-24 15:39:08 +0200

    fin exercices 3 php

--- 03-WEB/PHP/php_intro_exercices/1-hello-php/strings.php
+++ 03-WEB/PHP/php_intro_exercices/1-hello-php/strings.php
@@ -2,20 +2,38 @@
 
 function getMC2(string $name): string
 {
-    return "$name";
+
+$nom = strrchr($name, ' ');
+    return "$nom";
 }
-echo getMC2("Albert Einstein") . PHP_EOL;
+echo getMC2("Albert Einstein") . "<br>";
 
 
 
 function getUserName(string $nom, $prenom):string
 {
-    return "$nom, $prenom";
+    return "$nom  $prenom";
 }
-echo getUserName("Vitu", "Allan") . PHP_EOL;
+echo getUserName("Vitu", "Allan") . "<br>";
 
 
-function getFullName(string $nameMinu, $nameMaju):string
+
+function getFullName(string $prenom, string $nom):string
 {
-    
+     return ucfirst(strtolower($prenom)) . ' ' . strtoupper($nom);
 }
+echo getFullName('allan', 'vitu') . "<br>";
+
+
+
+
+function askUser(string $_nom, string $_prenom): string
+{
+    $nom = getFullName( $_prenom, $_nom);
+    $science = getMC2("Albert Einstein");
+    $item = "Bonjour $nom. Connaissez vous $science ?";
+
+    return $item;
+}
+
+echo askUser('Chatelot', 'Franck');
