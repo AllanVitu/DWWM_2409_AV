ca7e945d7d0086688b110ff4ee919ccf4a178b9e
Drawk <166002323+AllanVitu@users.noreply.github.com>
2025-05-06 15:58:33 +0200

    CoursPHP_Globale

---
+++ 03-WEB/PHP/php_intro_exercices/php_ex/README.md
@@ -0,0 +1,20 @@
+**EXERCICE 6.B Date et Heure**
+
+\*Le programme doit calculer et renvoyer la difference entre la date d'aujourd'hui
+et les dates futurs ou passé.\*
+
+SI (le programme lis 2019-09-29){
+    ALORS il retourne "Evenement passé";
+}SINON{
+    il retourne "Mauvais format de Date";
+}
+
+---------------------------------------------------------------------
+
+SI(Le programme = laDateAjd){
+    ALORS il retourne "Aujourd'hui";
+}SINON{
+    il retourne "Mauvais format de Date";
+}
+
+---------------------------------------------------------------------
--- 03-WEB/PHP/php_intro_exercices/php_ex/date_et_heure.php
+++ 03-WEB/PHP/php_intro_exercices/php_ex/date_et_heure.php
@@ -2,22 +2,51 @@
 
 //EX : 6.A
 
-function getToday():string
+function getToday(): string
 {
     $date = new DateTime('now');
     $date2 = $date->format('d/m/Y');
     return $date2;
 }
-echo getToday();
+echo getToday() . PHP_EOL;
 
 //EX: 6.B
 
 
-function getTimeLeft(string $chaineDate):string
+function getTimeLeft(string $chaineDate): string
 {
 
-    $tabDate = explode("-", $chaineDate);
-    
+   /* $tabDate = explode("-", $chaineDate);
+    if ($tabDate[0] > 0) {
+        if ($tabDate[1] > 0 && $tabDate[1] <= 12) {
+            switch($tabDate[1]){
+                case 1:
+                    if($tabDate[2] > 0 && $tabDate[2] <= 31){
 
-}    
-?>
+                    }else{
+                        return "Mauvais forat de date jours.";
+                    }break;
+
+            }
+            
+
+
+
+
+        } else {
+            return "Mauvais format de date mois.";
+        }
+    } else {
+        return "Mauvais format de date annee.";
+    }*/
+    try {
+        $date = new DateTime($chaineDate);
+        return $date -> format('d/m/Y');
+        
+
+    } catch (Exception $e) {
+        return  "Mauvais format de Date !";
+    }
+}
+echo getTimeLeft("2000-01-01") . PHP_EOL;
+echo getTimeLeft("2025-01-32") . PHP_EOL;
