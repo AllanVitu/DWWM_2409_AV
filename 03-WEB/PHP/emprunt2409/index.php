be1800e5c5801754541cf170c1c5b6757d01bcd2
Drawk <166002323+AllanVitu@users.noreply.github.com>
2025-05-12 12:33:32 +0200

    progression emprunt_2409

--- 03-WEB/PHP/emprunt2409/index.php
+++ 03-WEB/PHP/emprunt2409/index.php
@@ -1,22 +1,30 @@
-
 <!DOCTYPE html>
 <html lang="fr-FR">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Affichage tableau amortissement Pret</title>
+    <title>Affichage tableau amortissement Prêt</title>
 </head>
+
 <body>
     <?php
-    include "./models/Pret.php";
-    
+    include "./models/Pret..php";
+
     $objPret = new Pret(10000, 5.3, 5);
-    
-    echo $objPret->calculMensualite2();
-    
-    $tableauAmrt = $objPret->getTableauAmortissement();
-    
-    var_export($tableauAmrt);
+
+    echo $objPret->calculMensualite2() . "<br>";
+
+    // $tableauAmrt= $objPret->getTableauAmortissement();
+    // var_export($tableauAmrt);
+
+    $chainetab = $objPret->getTableauAmortissementJSON();
+    echo $chainetab;
+    $objPret->getreportJSON();
+    $objPret2 = new Pret(150000, 2.4, 15);
+    $objPret2->getreportJSON();
+
     ?>
 </body>
+
 </html>
--- 03-WEB/PHP/emprunt2409/models/Pret.php
+++ 03-WEB/PHP/emprunt2409/models/Pret.php
@@ -1,37 +1,32 @@
-<?php
-
-class Pret
+<?php class Pret
 {
-    // Attributs
 
-    public float $capital;
+    // attributs
+    private float $capital;
     private float $tauxMensuel;
     private int $nbMois;
 
-    // Propriétés / Accesseurs
-
+    // propriétés
+    //accesseurs
     public function getCapital(): float
     {
         return $this->capital;
     }
-
     public function getTauxMensuel(): float
     {
+
         return $this->tauxMensuel;
     }
 
-    public function getNbMois(): int
+    public function getMois(): int
     {
         return $this->nbMois;
     }
-
-    // Modifieur
-
-    public function setMois(int $nbnewmois): void
+    //modifieur
+    public function setMois(int $nbnouvmois): void
     {
-        $this->nbMois = $nbnewmois;
+        $this->nbMois = $nbnouvmois;
     }
-
     // Constructeur
 
     public function __construct(float $_montant, float $_tauxAnnuel, int $_nbAnnees)
@@ -41,13 +36,12 @@ class Pret
         $this->nbMois = $_nbAnnees * 12;
     }
 
-    //Methodes
+    // methodes
 
-    /*
-    Q= (1-(1+ Tm )puissance -n)
-    a : la mensualité constante recherché :
-    a= (k * tm)/Q
-    */
+    /*Q= (1-(1+ Tm )puissance -n)
+a : la mensualité constante recherché :
+a= (K x Tm)/Q*/
+
 
     public function calculMensualite(): float
     {
@@ -55,7 +49,8 @@ class Pret
         $mensualite = ($this->capital * $this->tauxMensuel) / $Q;
         return round($mensualite, 2);
     }
-    
+
+    //methode pour affichage
     public function calculMensualite2(): string
     {
         $Q = (1 - pow((1 + $this->tauxMensuel), -$this->nbMois));
@@ -71,28 +66,92 @@ class Pret
         $partAmortissement = 0;
         $mensualite = $this->calculMensualite();
         $capitalRestant = $this->capital;
-
         for ($i = 0; $i < $this->nbMois; $i++) {
+
 
             $partInteret = $capitalRestant * $this->tauxMensuel;
             $partAmortissement = $mensualite - $partInteret;
-
             if ($i > 0) {
+                $capitalRestant -= $partAmortissement;
+            }
+            $numeroMois = $i++;
 
+            Array_push($data,  ["num_mois" => $numeroMois, "partInteret" => round($partInteret, 2), "partAmortissement" => round($partAmortissement, 2), "capital_restant" => round($capitalRestant, 2), "mensualite" => round($mensualite, 2)]);
+        }
+        return $data;
+    }
+
+    public function getTableauAmortissementJSON(): string
+    {
+        $chaineJSON = "[";
+
+        $data = array();
+        $partInteret = 0;
+        $partAmortissement = 0;
+        $mensualite = $this->calculMensualite();
+        $capitalRestant = $this->capital;
+        for ($i = 0; $i < $this->nbMois; $i++) {
+
+
+            $partInteret = $capitalRestant * $this->tauxMensuel;
+            $partAmortissement = $mensualite - $partInteret;
+            if ($i > 0) {
                 $capitalRestant -= $partAmortissement;
             }
 
-            $numeroMois = $i++;
 
-            Array_push($data,  [
-                "numeroMois" => $i + 1,
-                "partInteret" => round($partInteret, 2),
-                "partAmortissement" => round($partAmortissement, 2),
-                "capital_restant" => round($capitalRestant, 2),
-                "mensualite" => round($mensualite, 2)
-            ]);
+            Array_push($data,  ["num_mois" => $i + 1, "partInteret" => round($partInteret, 2), "partAmortissement" => round($partAmortissement, 2), "capital_restant" => round($capitalRestant, 2), "mensualite" => round($mensualite, 2)]);
         }
 
-        return $data;
+        for ($i = 0; $i < count($data); $i++) {
+
+
+            if ($i < count($data) - 1) {
+                $chaineJSON .= json_encode($data[$i]) . ",";
+            } else {
+                $chaineJSON .= json_encode($data[$i]);
+            }
+        }
+        $chaineJSON .= "]";
+
+
+
+        return $chaineJSON;
+    }
+    public function getreportJSON(): void
+    {
+        $chaineJSON = " [";
+
+        $data = array();
+        $partInteret = 0;
+        $partAmortissement = 0;
+        $mensualite = $this->calculMensualite();
+        $capitalRestant = $this->capital;
+        for ($i = 0; $i < $this->nbMois; $i++) {
+
+
+            $partInteret = $capitalRestant * $this->tauxMensuel;
+            $partAmortissement = $mensualite - $partInteret;
+            if ($i > 0) {
+                $capitalRestant -= $partAmortissement;
+            }
+
+
+            Array_push($data,  ["num_mois" => $i + 1, "partInteret" => round($partInteret, 2), "partAmortissement" => round($partAmortissement, 2), "capital_restant" => round($capitalRestant, 2), "mensualite" => round($mensualite, 2)]);
+        }
+
+        for ($i = 0; $i < count($data); $i++) {
+
+
+            if ($i < count($data) - 1) {
+                $chaineJSON .= json_encode($data[$i], JSON_PRETTY_PRINT) . ",";
+            } else {
+                $chaineJSON .= json_encode($data[$i], JSON_PRETTY_PRINT);
+            }
+        }
+        $chaineJSON .= "]";
+
+
+        file_put_contents("./report/simulationPret.json", $chaineJSON);
     }
 }
